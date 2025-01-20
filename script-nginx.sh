7555c63d100abca0b9ee787716f88184162ccdc7
Ubuntu <ubuntu@ip-172-31-6-92.sa-east-1.compute.internal>
2025-01-20 17:45:07 -0300

    criado script para monitoramento nginx

---
+++ script-nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# var
+TIMESTAMP=$(date '+%d-%m-%Y %H:%M:%S')
+SERVICE_NAME="Nginx"
+
+# status service
+if systemctl is-active --quiet nginx; then
+    STATUS="ONLINE"
+    MESSAGE="O serviço $SERVICE_NAME esta ativo."
+    echo "$TIMESTAMP | $SERVICE_NAME | $STATUS | $MESSAGE" >> /home/ubuntu/estagio_compassuol/active_service.log
+else
+    STATUS="INATIVO"
+    MESSAGE="O serviço $SERVICE_NAME está inativo."
+    echo "$TIMESTAMP | $SERVICE_NAME | $STATUS | $MESSAGE" >> /home/ubuntu/estagio_compassuol/inactive_service.log
+fi
